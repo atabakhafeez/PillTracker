917361f538f5a0d473dab4edae489372f81dcbfe
atabakhafeez <atabakhafeez@hotmail.com>
2016-11-19 20:55:40 +0100

    The user can now give time intervals for pills

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var request = require('request');
 // var prompt = require('prompt');
 var prompt = require('prompt-sync')();
+var moment = require('moment');
 
 request({
 	url: 'http://lauzhack.ael.li/events',
@@ -26,14 +27,54 @@ request({
 function getIntervals() {
 	var pillIntervals = [];
 	var n = prompt('Please enter the number of times you must take medicine in one day: ');
-	for(var i = 0; i < n; i++) {
+	var regex = /^([0-9]|0[0-9]|1?[0-9]|2[0-3]):[0-5]?[0-9]$/g
+		for(var i = 0; i < n; i++) {
 		console.log('Interval # ' + i);
-		var startTime = prompt('Start [hh:mm]: ');
-		var endTime = prompt('End [hh:mm]: ');
+		
 		pillIntervals.push({
-			start: startTime,
-			end: endTime 
+			start: getStartTime(),
+			end: getEndTime()
 		});
 	}
 	console.log(pillIntervals);
 }
+
+function getStartTime() {
+	var regex = /^([0-9]|0[0-9]|1?[0-9]|2[0-3]):[0-5]?[0-9]$/g
+	var startTime = prompt('Start [hh:mm]: ');
+	while (!regex.test(startTime)) {
+		console.log('START: Not proper time format. Please enter again!');
+		startTime = null;
+		startTime = prompt('Start [hh:mm]: ');
+	}
+	console.log(startTime);
+	var parts = startTime.split(":");
+	var hours = parseInt(parts[0]);
+	var minutes = parseInt(parts[1]);
+	var seconds = (hours + minutes * 60) * 60;
+	return moment().startOf('day').unix() + seconds;
+}
+
+function getEndTime() {
+	var regex = /^([0-9]|0[0-9]|1?[0-9]|2[0-3]):[0-5]?[0-9]$/g
+	var endTime = prompt('End [hh:mm]: ');
+	regex = /^([0-9]|0[0-9]|1?[0-9]|2[0-3]):[0-5]?[0-9]$/g
+	while (!regex.test(endTime)) {
+		console.log('END: Not proper time format. Please enter again!');
+		endTime = null;
+		endTime = prompt('End [hh:mm]: ');
+	}
+	console.log(endTime);
+	var parts = endTime.split(":");
+	var hours = parseInt(parts[0]);
+	var minutes = parseInt(parts[1]);
+	var seconds = (hours + minutes * 60) * 60;
+	return moment().startOf('day').unix() + seconds;
+}
+
+
+
+
+
+
+
