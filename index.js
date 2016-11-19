7ab72f85e056bc1320f6477af019ff1be2681118
atabakhafeez <atabakhafeez@hotmail.com>
2016-11-19 17:50:43 +0100

    Take user input for time intervals

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var request = require('request');
-
+// var prompt = require('prompt');
+var prompt = require('prompt-sync')();
 
 request({
 	url: 'http://lauzhack.ael.li/events',
@@ -18,6 +19,21 @@ request({
     eventsResponse = JSON.parse(body);
     //this should be 1000
     console.log(eventsResponse.length);
+    getIntervals();
 
 });
 
+function getIntervals() {
+	var pillIntervals = [];
+	var n = prompt('Please enter the number of times you must take medicine in one day: ');
+	for(var i = 0; i < n; i++) {
+		console.log('Interval # ' + i);
+		var startTime = prompt('Start [hh:mm]: ');
+		var endTime = prompt('End [hh:mm]: ');
+		pillIntervals.push({
+			start: startTime,
+			end: endTime 
+		});
+	}
+	console.log(pillIntervals);
+}
