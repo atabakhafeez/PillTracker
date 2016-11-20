875259818449fb5287ad633b71fdfb4f809cafac
Temirlan <t.ulugbekuulu@jacobs-university.de>
2016-11-20 11:55:09 +0100

    Corrected timings, UK time

--- index.js
+++ index.js
@@ -51,49 +51,48 @@ request({
             check_if_took(button_events, intervals);
         });
 
-    }, 3000);
+    }, 30000);
 
-    // check_if_took(button_events, intervals);
 });
 
 
 
 
 function check_if_took (data, intervals){
-    var time_now = Math.floor(Date.now() / 1000);
+    var time_now = Math.floor(Date.now() / 1000) + 3600;//UK time
     var counter = 0;
     console.log('Looking at data...');
     for (var i = 0; (i < data.length) && (counter < intervals.length); i++){
         if (time_now < intervals[counter].start){ //still not time
-            counter = intervals.length;
-            break;
+            return;
         }
+        var start_mins = (intervals[counter].start -moment().startOf('day').unix() - 3600) / 60;
+        var start_hours = Math.floor(start_mins/60);
+        var start_minutes = start_mins % 60;
+        if (start_minutes == 0){
+            start_minutes = '00';
+        }
+        var end_mins = (intervals[counter].end - moment().startOf('day').unix() - 3600) / 60;
+        var end_hours = Math.floor(end_mins/60);
+        var end_minutes = end_mins % 60;
+        if (end_minutes == 0){
+            end_minutes = '00';
+        }
+        var took_hours = Math.floor(((data[i].creationTimestamp - moment().startOf('day').unix())/60)/60);
+        var took_minutes = ((data[i].creationTimestamp - moment().startOf('day').unix())/60) % 60;
+        var took_minutes = Math.round(took_minutes);
         if (counter < intervals.length - 1){
             if (data[i].creationTimestamp > intervals[counter].end && data[i].creationTimestamp < intervals[counter+1].start){
                 console.log('You took it in wrong time');
-                var start_mins = (intervals[counter].start -moment().startOf('day').unix()) / 60;
-                var start_hours = Math.floor(start_mins/60);
-                var start_minutes = start_mins % 60;
-                if (start_minutes == 0){
-                    start_minutes = '00';
-                }
-                var end_mins = (intervals[counter].end - moment().startOf('day').unix()) / 60;
-                var end_hours = Math.floor(end_mins/60);
-                var end_minutes = end_mins % 60;
-                if (end_minutes == 0){
-                    end_minutes = '00'
-                }
-                var took_hours = Math.floor(((data[i].creationTimestamp - moment().startOf('day').unix())/60)/60);
-                var took_minutes = ((data[i].creationTimestamp - moment().startOf('day').unix())/60) % 60;
-                var took_minutes = Math.round(took_minutes);
                 console.log('Instead of taking between '+start_hours+':'+start_minutes+'-'+end_hours+':'+end_minutes+', you took at '+took_hours+':'+took_minutes); 
                 counter++;
             }
         }
         if ((intervals[counter].start <= data[i].creationTimestamp) && (intervals[counter].end >= data[i].creationTimestamp)){
             counter++;
+            console.log('You took pill for time interval: ' + start_hours+':'+start_minutes+'-'+end_hours+':'+end_minutes);
         } else if (data[i].creationTimestamp < intervals[counter].start){
-            console.log('Took pill in appropriate time');
+            console.log('Took pill at wrong time');
         }
     }
     if (counter != intervals.length)
@@ -128,7 +127,7 @@ function getStartTime() {
     var hours = parseInt(parts[0]);
     var minutes = parseInt(parts[1]);
     var seconds = (hours * 60 + minutes) * 60;
-    return moment().startOf('day').unix() + seconds;
+    return moment().startOf('day').unix() + seconds + 3600;
 }
 
 function getEndTime() {
@@ -145,5 +144,5 @@ function getEndTime() {
     var hours = parseInt(parts[0]);
     var minutes = parseInt(parts[1]);
     var seconds = (hours * 60 + minutes) * 60;
-    return moment().startOf('day').unix() + seconds;
+    return moment().startOf('day').unix() + seconds + 3600;
 }
